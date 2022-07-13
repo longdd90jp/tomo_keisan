2ae40b294c9179f085e8e97c4f14be61041abbc2
longdangdjv <long.dang@drjoy.vn>
2022-07-13 19:33:29 +0900

    update

---
+++ common_js.js
@@ -0,0 +1,31 @@
+function isBlank(valStr) {
+    if(valStr == undefined || valStr == null) return true;
+    return valStr.trim() == '';
+}
+function isNotUndefined(valStr) {
+    if(valStr == undefined || valStr == null) return false;
+    return true;
+}
+function getValueOfInput(tagObj) {
+    return tagObj.value;
+}
+function setValueOfInput(tagObj, value) {
+    tagObj.value = value;
+}
+function getTextOfTag(tagObj) {
+    return tagObj.innerText;
+}
+function setTextOfTag(tagObj, value) {
+    tagObj.innerText = value;
+}
+// calculate function
+function toDegree(degree) {
+    return degree * Math.PI / 180;
+}
+function trimByNumberDigit(number, digit) {
+    let fixedNumber = number.toFixed(6).toString();
+    if(fixedNumber.endsWith('000')) return parseFloat(fixedNumber);
+
+    let strNum = number.toString();
+    let arrPart = strNum.split('.');
+}
